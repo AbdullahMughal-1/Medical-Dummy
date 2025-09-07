456c55343a15aebe9fe0392f824e82cca89505b7
AbdullahMughal-1 <abdullahmughalxdev@gmail.com>
2025-09-07 22:14:21 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
-import Home from './components/home'
+import Home from './components/Home'
+
 import SpecialistsLayout from './components/SpeciallistLayout'
 import TestimonialSection from './components/TestimonialSection'
 import HealthLayout from './components/HealthLayout'
